9fe364db5a21a94531088d7f5e386f124487549a
Аронов Олег Юрьевич <AronovOU@ugsk.ru>
2021-07-11 23:22:04 +0600

    task_09

---
+++ logs/task_09/install.sh
@@ -0,0 +1,6 @@
+# sudo apt update
+# sudo apt install nginx
+# nano /etc/nginx/sites-enabled/default
+# curl -L https://toolbelt.treasuredata.com/sh/install-ubuntu-focal-td-agent4.sh | sh
+
+# kafka config: listener: PLAINTEXT..ip:9092
