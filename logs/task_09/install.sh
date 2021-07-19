3b7976ab710460df806df88152737bab4b5c3fa2
Аронов Олег Юрьевич <AronovOU@ugsk.ru>
2021-07-19 17:26:06 +0600

    logs_09

--- logs/task_09/install.sh
+++ logs/task_09/install.sh
@@ -1,6 +1,16 @@
 # sudo apt update
 # sudo apt install nginx
-# nano /etc/nginx/sites-enabled/default
+# nano /etc/nginx/sites-enabled/default full log paths
 # curl -L https://toolbelt.treasuredata.com/sh/install-ubuntu-focal-td-agent4.sh | sh
 
 # kafka config: listener: PLAINTEXT..ip:9092
+
+listeners=PLAINTEXT://10.114.0.4:9092
+
+advertised.listeners=PLAINTEXT://10.114.0.4:9092
+
+раскомментировать listener.security.protocol.map=
+
+
+#curl -XDELETE localhost:9200/logstash-2021.07.19
+#bin/kafka-console-consumer.sh --bootstrap-server 10.114.0.4:9092 --topic topic --from-beginning
